d8870885d0a7ba211ce99de10b34e6d4b6aa1058
willluciano <williamluciano47@gmail.com>
2021-09-22 20:28:54 -0400

    changes to .JS and fix errors

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,64 +1,44 @@
-//Wait until Jquery is ready
-$( document ).ready(function() {
-  //Display current date
-  //Variable to append cols
-var a= moment().format('dddd MMMM Do YYYY, h:mm');
-  $("#currentDay").text(a)
-  var row = ""
-  //loop to dispaly 9am-18pm
-    for (var i= 9 ; i<=18; i++){
-      // Creation of the row elements
-      row = $(`<div class="row">`)
-      col1 = $(`<div class ="col-lg-2 hour">${displayAmorPm(i)}</div>`)
-      col2 = $(`<div class ="col-lg-8 inputcontent"><input data-input="${i}" id="inputText${i}" class="form-control inputText" type="text" name="userInput"></div>`)
-      col3 = $(`<div class ="col-lg-2"><button data-id="${i}" id="savePlanner" class="btn btn-success btn-block"><i class="fas fa-save"></i> Save</button></div>`)
-      row.append(col1)
-      row.append(col2)
-      row.append(col3)
-      $("#display-planner").append(row)
-      getlocalStorage(i)
-    }
-   $("button.btn.btn-success").click(function(e){
-   var id = $(this).data("id")
-   var inputText = $(this).parent().siblings().find("input").val()
-   localStorage.setItem(id,inputText)
-   })
-  //  Convert Am to Pm
-   function displayAmorPm(hour){
-     var b=""
-     if(hour<=12){
-       b= "AM"
-     }else{
-       b="PM"
-     }
-     hour = hour % 12
-     hour = hour ? hour : 12
-     return hour + " " + b
-   }
-  });
+$(document).ready(function () {
 
-   function getlocalStorage(hour){
-     let inputval = localStorage.getItem(hour)
-     if(true){
-      //  $("input").data(`input${hour}`)
-      var text= $(`input#inputText${hour}`).val(inputval)
-      console.log(text)
-     }
-   }
-   //Apdate color
-   function updateColor(){
-     var hour = new Date().getHours();
-     for (var i= 9 ; i<=18; i++){
-       console.log(hour,i)
-       if(hour==i ) {
-        $(`#inputText${i}`).css("background","red")
-       }else  if(hour<i ){
-        
-         $(`#inputText${i}`).css("background","lightblue")
-
-       }
-     }
-   }
-   setInterval(function(){
-     updateColor()
-   },1000)
+  // Displays the current date in the header
+  var today = moment();
+  $("#currentDay").text(today.format("LLLL"));
+  $(".saveBtn").on("click", function () {
+      var textValue = $(this).siblings(".description").val();
+      var timeKey = $(this).parent().attr("id");
+      localStorage.setItem(timeKey, textValue);
+  })
+  
+  $("#9 .description").val(localStorage.getItem("9"));
+  $("#10 .description").val(localStorage.getItem("10"));
+  $("#11 .description").val(localStorage.getItem("11"));
+  $("#12 .description").val(localStorage.getItem("12"));
+  $("#13 .description").val(localStorage.getItem("13"));
+  $("#14 .description").val(localStorage.getItem("14"));
+  $("#15 .description").val(localStorage.getItem("15"));
+  $("#16 .description").val(localStorage.getItem("16"));
+  $("#17 .description").val(localStorage.getItem("17"));
+  
+  
+  function cssUpdate () {
+     var currentHour = today.hours();
+     $(".time-block").each(function () {
+         var divTimeId = parseInt($(this).attr("id"));
+          console.log(divTimeId);
+         if (divTimeId < currentHour) {
+             $(this).addClass("past");
+         } else if (divTimeId === currentHour) {
+             $(this).removeClass("past");
+             $(this).removeClass("future");
+             $(this).addClass("present");
+         } else {
+             $(this).removeClass("past");
+             $(this).removeClass("present");
+             $(this).addClass("future");
+         }
+     })
+  }
+  
+  cssUpdate ();
+  
+  })
